600825984cbfbd838a35aa5af59e63ca952f3f7b
Kevin Brothaler <contact@digipom.com>
2016-05-19 16:16:40 -0400

    Add a data test.

--- tests/lib.rs
+++ tests/lib.rs
@@ -52,3 +52,20 @@ fn test_header_for_extended_file_32bit_stereo_44100() {
 	assert_eq!(44100, wave_reader.pcm_format.sample_rate);
 	assert_eq!(32, wave_reader.pcm_format.bits_per_sample);
 }
+
+// MARK: Data tests
+
+#[test]
+fn test_header_for_standard_file_8bit_mono_16000_with_data() {
+	let file = File::open("tests/pcm_8bit_mono_16000_standard_with_data.wav").unwrap();
+	let mut wave_reader = WaveReader::new(file).unwrap();
+	assert_eq!(1, wave_reader.pcm_format.num_channels);
+	assert_eq!(16000, wave_reader.pcm_format.sample_rate);
+	assert_eq!(8, wave_reader.pcm_format.bits_per_sample);
+
+	let mut buffer = [0; 16];
+	wave_reader.read_samples_as_u8(&mut buffer).unwrap();
+	for (i, &val) in buffer.iter().enumerate() {
+        assert_eq!(i as u8, val);
+    }
+}
