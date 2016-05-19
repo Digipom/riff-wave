f1325b048b8a2b8d76b75f397ccc8aaed8bfeea2
Kevin Brothaler <contact@digipom.com>
2016-05-19 10:25:22 -0400

    Add cb test.

--- tests/lib.rs
+++ tests/lib.rs
@@ -30,3 +30,12 @@ fn test_header_for_standard_file_16bit_stereo_44100() {
 	assert_eq!(44100, wave_reader.pcm_format.sample_rate);
 	assert_eq!(16, wave_reader.pcm_format.bits_per_sample);
 }
+
+#[test]
+fn test_header_for_standard_file_16bit_stereo_44100_with_cb() {
+	let file = File::open("tests/pcm_16bit_stereo_44100_standard_with_cb.wav").unwrap();
+	let wave_reader = WaveReader::new(file).unwrap();
+	assert_eq!(2, wave_reader.pcm_format.num_channels);
+	assert_eq!(44100, wave_reader.pcm_format.sample_rate);
+	assert_eq!(16, wave_reader.pcm_format.bits_per_sample);
+}
